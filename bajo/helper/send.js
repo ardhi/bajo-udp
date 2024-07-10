28ffa28ae4070454e1027e289448126cde742022
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-10 11:23:03 +0700

    Update send.js

--- bajo/helper/send.js
+++ bajo/helper/send.js
@@ -2,8 +2,8 @@ async function send ({ msg, to } = {}) {
   const { error } = this.app.bajo.helper
   const { find } = this.app.bajo.helper._
   const { addressSplit } = this.app.bajoEmitter.helper
-  const { connection, transport } = addressSplit(to)
-  if (transport !== 'bajoUdp') return
+  const { connection, plugin } = addressSplit(to)
+  if (plugin !== 'bajoUdp') return
   const c = find(this.connections, { name: connection })
   if (!c) throw error('No such connection \'%s\'', connection)
   c.instance.send(msg, c.port, c.host)
