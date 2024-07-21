c0b05c3720cb1d55de61ec971390e8b2aba5aca0
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-21 07:25:10 +0700

    Refactor: bajo-emitter is now optional

---
+++ bajo/init.js
@@ -0,0 +1,13 @@
+async function handler ({ item, options }) {
+  const { has } = this.app.bajo.lib._
+  if (!has(item, 'port')) throw this.error('Connection must have a port')
+  item.host = item.host ?? '0.0.0.0'
+  item.server = item.server ?? false
+}
+
+async function init () {
+  const { buildCollections } = this.app.bajo
+  this.connections = await buildCollections({ ns: this.name, handler, dupChecks: ['name', 'port'] })
+}
+
+export default init
--- bajo/start.js
+++ bajo/start.js
@@ -1,23 +1,15 @@
 import dgram from 'dgram'
 
-async function handler ({ item, options }) {
-  const { has } = this.app.bajo.lib._
-  if (!has(item, 'port')) throw this.error('Connection must have a port')
-  item.host = item.host ?? '0.0.0.0'
-  item.server = item.server ?? false
-}
-
 async function start () {
-  const { emit } = this.app.bajoEmitter
-  const { buildCollections } = this.app.bajo
-
-  this.connections = await buildCollections({ ns: this.name, handler, dupChecks: ['name', 'port'] })
+  const { runHook } = this.app.bajo
+  const { camelCase } = this.app.bajo.lib._
 
   for (const c of this.connections ?? []) {
     const socket = dgram.createSocket({ type: 'udp4', reuseAddr: c.reuseAddr ?? false })
     for (const evt of this.events) {
       socket.on(evt, async (...args) => {
-        emit(`bajoUdp.${evt}`, c, ...args)
+        if (this.app.bajoEmitter) this.app.bajoEmitter.emit(`${this.name}.${evt}`, c, ...args)
+        await runHook(`${this.name}:${camelCase('on ' + evt)}`, c, ...args)
       })
     }
     if (c.server) socket.bind(c.port, c.host)
